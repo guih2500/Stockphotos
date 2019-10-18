a421d7df28973e8a34312af143b58589f52f6cff
guih2500 <gui.lima2500@gmail.com>
2019-10-18 01:05:51 -0300

    Novo Commit

---
+++ WebApplication/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            DBContext context = HttpContext.RequestServices.GetService(typeof(DBContext)) as DBContext;
+            List<cadastro> list = context.GetAllCadastro();
+
+            return View(list);
+        }
+    }
+}
---
+++ WebApplication/DBContext.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication
+{
+    public class DBContext
+    {
+        public string ConnectionString { get; set; }
+
+        public DBContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<cadastro> GetAllCadastro()
+        {
+            List<cadastro> list = new List<cadastro>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from usuarios", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new cadastro()
+                        {
+                            user_email = reader["user_email"].ToString(),
+                            user_senha = reader["user_senha"].ToString()
+                        });
+                    }
+                }
+                return list;
+            }
+        }
+
+    }
+}
